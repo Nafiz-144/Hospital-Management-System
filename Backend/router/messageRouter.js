00564b5471dbe29bccc7424109a3a92a3e5078ef
Nafiz144 <nafizahamed0464@gmail.com>
2024-09-14 02:15:42 +0600

    14-09-24

--- Backend/router/messageRouter.js
+++ Backend/router/messageRouter.js
@@ -1,12 +1,9 @@
 import express from "express";
-import {
-  getAllMessages,
-  sendMessage,
-} from "../controller/messageController.js";
-import { isAdminAuthenticated } from "../middlewares/auth.js";
+import { sendMessage } from "../controller/messageController.js";
+//import { isAdminAuthenticated } from "../middlewares/auth.js";
 const router = express.Router();
 
 router.post("/send", sendMessage);
-router.get("/getall", isAdminAuthenticated, getAllMessages);
+//router.get("/getall", isAdminAuthenticated, getAllMessages);
 
 export default router;
