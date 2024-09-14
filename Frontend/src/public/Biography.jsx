b1d748505ff6b9833e6bcd46b677c3fd041ead39
Nafiz144 <nafizahamed0464@gmail.com>
2024-09-14 23:16:24 +0600

    14-2

--- Frontend/src/public/Biography.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-const Biography = () => {
-  return <></>;
-};
-export default Biography;
--- Frontend/src/public/MessageForm.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-const MessageForm = () => {
-  return <></>;
-};
-export default MessageForm;
